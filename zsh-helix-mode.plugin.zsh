eb1820fa06f32b15c6b7f35f99d79456c739abf7
thomasschafer <thomasschafer97@gmail.com>
2025-01-16 07:25:53 +0000

    Add w, W, b, B, e and E

--- zsh-helix-mode.plugin.zsh
+++ zsh-helix-mode.plugin.zsh
@@ -42,6 +42,121 @@ function zhm_handle_backspace() {
     fi
 }
 
+function zhm_find_word_boundary() {
+    local direction=$1
+    local boundary=$2
+    local word_type=$3
+    local pos=$CURSOR
+    local len=$#BUFFER
+
+    local pattern is_match
+    if [[ $word_type == "WORD" ]]; then
+        pattern="[[:space:]]"
+        is_match="! [[ \$char =~ \$pattern ]]"  # Match non-space
+    elif [[ $word_type == "word" ]]; then
+        pattern="[[:alnum:]_]"
+        is_match="[[ \$char =~ \$pattern ]]"    # Match word chars
+    else
+        echo "Invalid word_type: $word_type" >&2
+        return 1
+    fi
+
+    if [[ $direction == "next" ]]; then
+        if [[ $boundary == "start" ]]; then
+            # Skip current word
+            while ((pos < len)); do
+                local char="${BUFFER:$pos:1}"
+                if eval $is_match; then
+                    ((pos++))
+                else
+                    break
+                fi
+            done
+            # Skip non-word chars
+            while ((pos < len)); do
+                local char="${BUFFER:$pos:1}"
+                if ! eval $is_match; then
+                    ((pos++))
+                else
+                    break
+                fi
+            done
+        elif [[ $boundary == "end" ]]; then
+            # Move at least one position forward if we're at the end of a word
+            if ((pos < len)); then
+                local char="${BUFFER:$pos:1}"
+                if eval $is_match; then
+                    ((pos++))
+                fi
+            fi
+            # Skip non-word chars
+            while ((pos < len)); do
+                local char="${BUFFER:$pos:1}"
+                if ! eval $is_match; then
+                    ((pos++))
+                else
+                    break
+                fi
+            done
+            # Skip to end of word
+            while ((pos < len)); do
+                local char="${BUFFER:$pos:1}"
+                if eval $is_match; then
+                    ((pos++))
+                else
+                    break
+                fi
+            done
+            # Move back one to land on last char
+            if ((pos > CURSOR)); then
+                ((pos--))
+            fi
+        else
+            echo "Invalid boundary: $boundary" >&2
+            return 1
+        fi
+    elif [[ $direction == "prev" ]]; then
+        if [[ $boundary == "start" ]]; then
+            # If on word char, move back one
+            if ((pos > 0)); then
+                local char="${BUFFER:$((pos-1)):1}"
+                if eval $is_match; then
+                    ((pos--))
+                fi
+            fi
+            # Skip non-word chars
+            while ((pos > 0)); do
+                local char="${BUFFER:$((pos-1)):1}"
+                if ! eval $is_match; then
+                    ((pos--))
+                else
+                    break
+                fi
+            done
+            # Skip word chars
+            while ((pos > 0)); do
+                local char="${BUFFER:$((pos-1)):1}"
+                if eval $is_match; then
+                    ((pos--))
+                else
+                    break
+                fi
+            done
+        elif [[ $boundary == "end" ]]; then
+            echo "Backward end motion not implemented" >&2
+            return 1
+        else
+            echo "Invalid boundary: $boundary" >&2
+            return 1
+        fi
+    else
+        echo "Invalid direction: $direction" >&2
+        return 1
+    fi
+
+    zhm_safe_cursor_move $pos
+}
+
 function zhm_handle_normal_mode() {
     case $KEYS in
         "i")
@@ -52,6 +167,24 @@ function zhm_handle_normal_mode() {
             ;;
         "l")
             zhm_safe_cursor_move $((CURSOR + 1))
+            ;;
+        "w")
+            zhm_find_word_boundary "next" "start" "word"
+            ;;
+        "W")
+            zhm_find_word_boundary "next" "start" "WORD"
+            ;;
+        "b")
+            zhm_find_word_boundary "prev" "start" "word"
+            ;;
+        "B")
+            zhm_find_word_boundary "prev" "start" "WORD"
+            ;;
+        "e")
+            zhm_find_word_boundary "next" "end" "word"
+            ;;
+        "E")
+            zhm_find_word_boundary "next" "end" "WORD"
             ;;
     esac
 }
